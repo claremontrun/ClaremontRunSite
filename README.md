2417e2bf4d60e445afb4a6d209b982e629ae491a
Sabrina Wasserman <sabrinawasserman@v1040-wn-rt-c-75-252.campus-dynamic.uwaterloo.ca>
2019-05-09 11:28:35 -0400

    Initial Commit. Site at launch

---
+++ README.md
@@ -0,0 +1,4 @@
+#ClaremontRunSite
+
+Website for the Claremont Run project, tracking Chris Claremont's run on the Uncanny X-Men.
+
---
+++ script/menu_controls.js
@@ -0,0 +1,7 @@
+function openSideMenu() {
+  document.getElementById("hamburgerMenu").style.width = "250px";
+}
+
+function closeSideMenu() {
+  document.getElementById("hamburgerMenu").style.width = "0";
+}
